46016eda02c36f20f7e1fcb9d5236ec9fed7d8ba
shun370 <mr0703hn@gmail.com>
2022-05-08 17:48:04 +0900

    add index.html add script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+for(let i = 1; i < 21; i += 1) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if(i % 3 === 0) {
+        console.log('Fizz');
+    } else if(i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
